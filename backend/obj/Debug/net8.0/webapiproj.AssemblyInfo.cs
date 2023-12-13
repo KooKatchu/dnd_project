fad309bf0631bf585b598e4c13ebb7b26491ec0b
KooKatchu <kookatchu@gmail.com>
2023-12-13 10:29:54 +0000

    Changed API to local host rather than flipbook while flipbook has issues

--- backend/obj/Debug/net8.0/webapiproj.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/webapiproj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapiproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b499cd48d59f23f0ff0d58620b277039d8d7671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a5136bd11a0ec81a06049f9e1fe7482bdf6f09b")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapiproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapiproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
